eba191bd96e18a93d85fbefca920887c275fc28a
azurecurve <github@azurecurve.co.uk>
2020-04-05 13:23:15 +0100

    * Fix bug with setting of default options.  * Fix bug with plugin menu.  * Update plugin menu css.

--- azrcrv-filtered-categories.php
+++ azrcrv-filtered-categories.php
@@ -3,7 +3,7 @@
  * ------------------------------------------------------------------------------
  * Plugin Name: Filtered Categories
  * Description: Creates a new Categories sidebar widget which allows categories to be included/excluded. A link to a categories page listing all categories can be configured to be displayed; a shortcode [fc] can be used on this page to display categories list.
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: azurecurve
  * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
  * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/filtered-categories/
@@ -24,7 +24,7 @@ if (!defined('ABSPATH')){
 
 // include plugin menu
 require_once(dirname( __FILE__).'/pluginmenu/menu.php');
-register_activation_hook(__FILE__, 'azrcrv_create_plugin_menu_fc');
+add_action('admin_init', 'azrcrv_create_plugin_menu_fc');
 
 // include update client
 require_once(dirname(__FILE__).'/libraries/updateclient/UpdateClient.class.php');
@@ -36,9 +36,7 @@ require_once(dirname(__FILE__).'/libraries/updateclient/UpdateClient.class.php')
  *
  */
 // add actions
-register_activation_hook(__FILE__, 'azrcrv_fc_set_default_options');
-
-// add actions
+add_action('admin_init', 'azrcrv_fc_set_default_options');
 add_action('admin_menu', 'azrcrv_fc_create_admin_menu');
 add_action('admin_post_azrcrv_fc_save_options', 'azrcrv_fc_save_options');
 add_action('widgets_init', 'azrcrv_fc_create_widget');
@@ -72,14 +70,17 @@ function azrcrv_fc_load_languages() {
 function azrcrv_fc_set_default_options($networkwide){
 	
 	$option_name = 'azrcrv-fc';
-	$old_option_name = 'azc_fc';
 	
 	$new_options = array(
-				'taxonomy' => 'category',
-				'category_page' => 'categories',
-				'category_link' => 'View all categories',
-				'category_page_count' => 0,
-				'category_page_feed_image' => '',
+						'taxonomy' => 'category',
+						'include_exclude' => 'include',
+						'category_page' => 'categories',
+						'category_link' => 'View all categories',
+						'category_page_show_count' => 0,
+						'category_page_feed_enabled' => 0,
+						'category_page_feed_image' => '',
+						'category' => array(''),
+						'updated' => strtotime('2020-04-04'),
 			);
 	
 	// set defaults for multi-site
@@ -94,20 +95,20 @@ function azrcrv_fc_set_default_options($networkwide){
 			foreach ($blog_ids as $blog_id){
 				switch_to_blog($blog_id);
 				
-				azrcrv_fc_update_options($option_name, $new_options, false, $old_option_name);
+				azrcrv_fc_update_options($option_name, $new_options, false);
 			}
 
 			switch_to_blog($original_blog_id);
 		}else{
-			azrcrv_fc_update_options( $option_name, $new_options, false, $old_option_name);
+			azrcrv_fc_update_options( $option_name, $new_options, false);
 		}
 		if (get_site_option($option_name) === false){
-			azrcrv_fc_update_options($option_name, $new_options, true, $old_option_name);
+			azrcrv_fc_update_options($option_name, $new_options, true);
 		}
 	}
 	//set defaults for single site
 	else{
-		azrcrv_fc_update_options($option_name, $new_options, false, $old_option_name);
+		azrcrv_fc_update_options($option_name, $new_options, false);
 	}
 }
 
@@ -117,30 +118,29 @@ function azrcrv_fc_set_default_options($networkwide){
  * @since 1.1.3
  *
  */
-function azrcrv_fc_update_options($option_name, $new_options, $is_network_site, $old_option_name){
+function azrcrv_fc_update_options($option_name, $new_options, $is_network_site){
 	if ($is_network_site == true){
 		if (get_site_option($option_name) === false){
-			if (get_site_option($old_option_name) === false){
-				add_site_option($option_name, $new_options);
-			}else{
-				add_site_option($option_name, azrcrv_fc_update_default_options($new_options, get_site_option($old_option_name)));
-			}
+			add_site_option($option_name, $new_options);
 		}else{
-			update_site_option($option_name, azrcrv_fc_update_default_options($new_options, get_site_option($option_name)));
+			$options = get_site_option($option_name);
+			if (!isset($options['updated']) OR $options['updated'] < $new_options['updated'] ){
+				$options['updated'] = $new_options['updated'];
+				update_site_option($option_name, azrcrv_fc_update_default_options($options, $new_options));
+			}
 		}
 	}else{
 		if (get_option($option_name) === false){
-			if (get_option($old_option_name) === false){
-				add_option($option_name, $new_options);
-			}else{
-				add_option($option_name, azrcrv_fc_update_default_options($new_options, get_option($old_option_name)));
-			}
+			add_option($option_name, $new_options);
 		}else{
-			update_option($option_name, azrcrv_fc_update_default_options($new_options, get_option($option_name)));
+			$options = get_option($option_name);
+			if (!isset($options['updated']) OR $options['updated'] < $new_options['updated'] ){
+				$options['updated'] = $new_options['updated'];
+				update_option($option_name, azrcrv_fc_update_default_options($options, $new_options));
+			}
 		}
 	}
 }
-
 
 /**
  * Add default options to existing options.
@@ -153,10 +153,10 @@ function azrcrv_fc_update_default_options( &$default_options, $current_options )
     $current_options = (array) $current_options;
     $updated_options = $current_options;
     foreach ($default_options as $key => &$value) {
-        if (is_array( $value) && isset( $updated_options[$key ])){
+        if (is_array( $value) && isset( $updated_options[$key])){
             $updated_options[$key] = azrcrv_fc_update_default_options($value, $updated_options[$key]);
         } else {
-            $updated_options[$key] = $value;
+			$updated_options[$key] = $value;
         }
     }
     return $updated_options;
@@ -245,8 +245,13 @@ function azrcrv_fc_display_options(){
 							$query = "SELECT t.term_id AS `term_id`, t.name AS `name` FROM $wpdb->term_taxonomy tt INNER JOIN $wpdb->terms t On t.term_id = tt.term_id WHERE tt.taxonomy = 'category' ORDER BY t.name";
 							$_query_result = $wpdb->get_results($query);
 							foreach($_query_result as $data){
+								if (isset($options['category'][$data->term_id])){
+									$selected_category = checked('1', $options['category'][$data->term_id]);
+								}else{
+									$selected_category = '';
+								}
 								?>
-								<label for="<?php echo $data->term_id; ?>"><input name="category[<?php echo $data->term_id; ?>]" type="checkbox" id="category" value="1" <?php checked('1', $options['category'][$data->term_id]); ?> /><?php echo esc_html($data->name); ?></label><br />
+								<label for="<?php echo $data->term_id; ?>"><input name="category[<?php echo $data->term_id; ?>]" type="checkbox" id="category" value="1" <?php echo $selected_category; ?> /><?php echo esc_html($data->name); ?></label><br />
 								<?php
 							}
 							unset($_query_result);
